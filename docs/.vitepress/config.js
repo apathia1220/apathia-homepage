79acba1ece34b5c98a039401f94c799f38dd7617
xuzhicheng <xuzhicheng@xuzhichengdeMacBook-Pro.local>
2022-08-05 16:36:36 +0800

    feat: init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,25 @@
+export default {
+  title: 'Apathia Component 中文文档',
+  description: '基于vue3 + typescript + twind 的vue组件库',
+  lang: 'zh-CN',
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: 'icon.png' }]],
+  themeConfig:{
+      siteTitle:'Apathia Component 中文文档',
+      repo: 'https://github.com/apathia1220/apathia-vue',
+      repoLabel:'GitHub',
+      docsDir: 'docs',
+      docsBranch: 'master',
+      lastUpdatedText: '最近更新时间',
+      logo: '/icon.png',
+      nav: [
+          { text: '相关文档', link: 'https://apathia1220.github.io/apathia-doc/' },
+      ],
+      socialLinks:[
+          { icon: 'github', link: 'https://github.com/apathia1220/apathia-vue' }
+      ],
+      // footer: {
+      //     message: 'MIT Licensed',
+      //     copyright: 'Copyright © 2022-present Apathia'
+      // }
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,31 @@
+---
+layout: home
+
+hero:
+  name: Apathia Component
+  text: 使用文档
+  tagline: 基于vue3 + typescript + twind 的vue组件库
+  image:
+    src: ./icon.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: https://apathia1220.github.io/apathia-doc/
+    - theme: alt
+      text: GitHub
+      link: https://github.com/apathia1220/apathia-vue
+features:
+  - icon: 🎉
+    details: ui 和 代码展示统一,ui 更友好
+  - icon: 📖
+    details: 展示代码语法高亮更清晰,组件名称汉化
+  - icon: 🔎
+    details: 使用typescript编写，类型完整，包含常用的composition api
+  - icon: ⚡️
+    details: 可自定义的全局样式
+  - icon: 🛠️
+    details: css-in-js 方便快捷
+  - icon: 💡
+    details: 可通过包管理器直接下载安装，并使用
+---
